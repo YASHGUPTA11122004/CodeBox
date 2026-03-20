80f91a53c714b4f1a838a87d38632eb4273d5d8e
Yash Gupta <yashgupta11122004@gmail.com>
2026-03-20 20:45:59 +0530

    fix allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(!*^c#qy$18ei1&!7pad(jktiw@5^_(o2s(4vxthe4_2e1rlbr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
